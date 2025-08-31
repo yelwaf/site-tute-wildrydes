e1cf14e4fc864c554d04974629e665a45d4556af
yelwaf <GlennFawley@hotmail.com>
2025-08-31 08:22:59 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,16 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_8QIDpfde7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1fg29dubh732n0de8985elis3m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_LlnILmsth', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72ktutefl7ahv6t67dvqdh3a43', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
         // SECRET_HASH: '1auv5m238ca4vu48kqcv4q8reel7pvs4octv2ugi9p6idikl2kla' // shhh
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-        SECRET_HASH: '1auv5m238ca4vu48kqcv4q8reel7pvs4octv2ugi9p6idikl2kla' // shhh
+     //   SECRET_HASH: '1auv5m238ca4vu48kqcv4q8reel7pvs4octv2ugi9p6idikl2kla' // shhh
     }
 };
+
 
 
 
