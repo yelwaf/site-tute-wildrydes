2f5a3964eb8865c9f727ac4fd7c5c5795d21f83a
yelwaf <GlennFawley@hotmail.com>
2025-08-31 08:11:54 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,12 +3,13 @@ window._config = {
         userPoolId: 'us-west-2_8QIDpfde7', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1fg29dubh732n0de8985elis3m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
-        SECRET_HASH: '1auv5m238ca4vu48kqcv4q8reel7pvs4octv2ugi9p6idikl2kla' // shhh
+        // SECRET_HASH: '1auv5m238ca4vu48kqcv4q8reel7pvs4octv2ugi9p6idikl2kla' // shhh
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
