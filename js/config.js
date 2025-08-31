86c3b16f5a8ad9a5cd91efe6ebd286f4f3305fd6
yelwaf <GlennFawley@hotmail.com>
2025-08-31 08:04:13 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_g9puC3Xoo', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3qrvuc3u5m81tm44936ks2gtt4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_8QIDpfde7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1fg29dubh732n0de8985elis3m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
